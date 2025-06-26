f5dd89b6468e05e9e0f7e9a12a2baf9323b622dd
rinku <rinkukumardas133@gmail.com>
2025-06-26 21:35:05 +0530

    3rd day working on omdbapi

--- react-movie-db/src/components/NavBar.js
+++ react-movie-db/src/components/NavBar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import "../style/NavBar.css"
 
+
+const API_BASE_URL = https://www.omdbapi.com/?s=avatar&apikey=b1cb7932;
+
 export default function NavBar() {
   return (
     <div className="navbar">
